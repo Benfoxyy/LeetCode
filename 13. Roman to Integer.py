9cda28ebed2b306943cade2706c108554a4b19a7
Ben <146076866+Benfoxyy@users.noreply.github.com>
2024-11-08 22:46:47 +0330

    13. Roman to Integer

---
+++ 13. Roman to Integer.py
@@ -0,0 +1,10 @@
+dic = {'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900, 'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+s = input("Enter a Roman numeral: ")
+output = 0
+
+for i in dic.keys():
+    while i in s:
+        output += dic[i]
+        s = s.replace(i, '', 1)
+
+print(f"Final Output: {output}")
