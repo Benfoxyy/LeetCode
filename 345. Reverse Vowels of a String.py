cf0506dbc21aba3b6345190f73828ce088cd04ab
Ben <146076866+Benfoxyy@users.noreply.github.com>
2025-02-09 22:15:06 +0330

    Update 345. Reverse Vowels of a String.py

--- 345. Reverse Vowels of a String.py
+++ 345. Reverse Vowels of a String.py
@@ -1,13 +1,17 @@
-str1 = "ABCABC"
-str2 = "ABC"
+s = "IceCreAm"
 
 class Solution:
-    def gcdOfStrings(self, str1: str, str2: str) -> str:
-        # Check if concatenated strings are equal or not, if not return ""
-        if str1 + str2 != str2 + str1:
-            return ""
-        # If strings are equal than return the substring from 0 to gcd of size(str1), size(str2)
-        from math import gcd
-        return str1[:gcd(len(str1), len(str2))]
-
-print(Solution().gcdOfStrings(str1,str2))
+    def reverseVowels(self, s: str) -> str:
+        t = []
+        for i in s:
+            if i in 'aAeEiIoOuU':
+                s = s.replace(i,'*',1)
+                t.append(i)
+        for j in s:
+            if j == '*':
+                s = s.replace(j,t.pop(),1)
+    
+        return s
+            
+                
+print(Solution().reverseVowels(s))
